eaff56dcd04ca53c88fa97414d550c18c5ab53b6
Nicola Spanu <nicspanu@gmail.com>
2020-11-16 12:21:47 +0100

    8

---
+++ pageobjects/searchtest.py
@@ -0,0 +1,17 @@
+import unittest
+from pageobjects.pages.homepage import HomePage
+from pageobjects.base.basetestcase import BaseTestCase
+
+
+class SearchProductTest(BaseTestCase):
+    def testSearchForProduct(self):
+        homepage = HomePage(self.driver)
+        search_results = homepage.search.searchFor('earphones')
+        self.assertEqual(3, search_results.product_count)
+        product = search_results.open_product_page('MADISON EARBUDS')
+        self.assertEqual('MADISON EARBUDS', product.name)
+        self.assertEqual('$35.00', product.price)
+        self.assertEqual('IN STOCK', product.stock_status)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
